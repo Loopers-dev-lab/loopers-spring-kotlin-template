9e17032d479ddeacd5fba2bbaef41347a37a9c42
jikimee64 <jikimee64@gmail.com>
2025-10-31 20:15:56 +0900

    feat: 회원가입, 내 정보 조회 서비스 기능 구현 및 통합 테스트 추가

---
+++ apps/commerce-api/src/main/kotlin/com/loopers/infrastructure/user/UserRepositoryImpl.kt
@@ -0,0 +1,24 @@
+package com.loopers.infrastructure.user
+
+import com.loopers.domain.user.User
+import com.loopers.domain.user.UserId
+import com.loopers.domain.user.UserRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+class UserRepositoryImpl(
+    private val userJpaRepository: UserJpaRepository,
+) : UserRepository {
+
+    override fun save(user: User): User {
+        return userJpaRepository.save(user)
+    }
+
+    override fun exist(userId: String): Boolean {
+        return userJpaRepository.existsByUserId(UserId(userId))
+    }
+
+    override fun getBy(userId: String): User? {
+        return userJpaRepository.findByUserId(UserId(userId))
+    }
+}
---
+++ apps/commerce-api/src/test/kotlin/com/loopers/domain/user/UserServiceTest.kt
@@ -0,0 +1,108 @@
+package com.loopers.domain.user
+
+import com.loopers.IntegrationTest
+import com.loopers.support.error.CoreException
+import com.loopers.support.error.ErrorType
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.assertj.core.api.SoftAssertions.assertSoftly
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean
+
+class UserServiceTest : IntegrationTest() {
+
+    @MockitoSpyBean
+    private lateinit var userRepository: UserRepository
+
+    @Autowired
+    private lateinit var userService: UserService
+
+    @DisplayName("UserService SingUp")
+    @Nested
+    inner class SignUp {
+        @Test
+        fun `회원 가입시 User 저장이 수행된다`() {
+            // given
+            val command = createSignUpCommand()
+
+            // when
+            val user = userService.signUp(command)
+
+            // then
+            assertSoftly { softly ->
+                softly.assertThat(user).isNotNull
+                softly.assertThat(user.userId.value).isEqualTo(command.userId)
+                softly.assertThat(user.email.value).isEqualTo(command.email)
+                softly.assertThat(user.birthDate.value).isEqualTo(command.birthDate)
+                softly.assertThat(user.gender).isEqualTo(command.gender)
+            }
+            verify(userRepository, times(1)).save(any())
+            verify(userRepository, times(1)).exist(any())
+        }
+
+        @Test
+        fun `이미 가입된 userId로 회원가입 시도시 실패한다`() {
+            // given
+            val command = createSignUpCommand()
+
+            // when
+            doReturn(true).`when`(userRepository).exist(command.userId)
+
+            // then
+            assertThatThrownBy {
+                userService.signUp(command)
+            }.isInstanceOfSatisfying(CoreException::class.java) { error ->
+                assertThat(error.errorType).isEqualTo(ErrorType.CONFLICT)
+            }
+            verify(userRepository, times(1)).exist(any())
+        }
+    }
+
+    @DisplayName("UserService MyInfo")
+    @Nested
+    inner class MyInfo {
+        @Test
+        fun `해당 ID 의 회원이 존재할 경우, 회원 정보가 반환된다`() {
+            // given
+            val command = createSignUpCommand()
+            val signUpUser = userService.signUp(command)
+
+            // when
+            val user = userService.getMyInfo(signUpUser.userId.value)
+
+            // then
+            assertSoftly { softly ->
+                softly.assertThat(user).isNotNull
+                softly.assertThat(user!!.userId.value).isEqualTo(command.userId)
+                softly.assertThat(user.email.value).isEqualTo(command.email)
+                softly.assertThat(user.birthDate.value).isEqualTo(command.birthDate)
+                softly.assertThat(user.gender).isEqualTo(command.gender)
+            }
+        }
+
+        @Test
+        fun `해당 ID 의 회원이 존재하지 않을 경우, null 이 반환된다`() {
+            // given & when
+            val user = userService.getMyInfo("nonUserId")
+
+            // then
+            assertSoftly { softly ->
+                softly.assertThat(user).isNull()
+            }
+        }
+    }
+
+    private fun createSignUpCommand(
+        userId: String = "testUserId",
+        email: String = "test@example.com",
+        birthDate: String = "2000-01-01",
+        gender: Gender = Gender.MALE,
+    ) = UserCommand.SignUp(userId, email, birthDate, gender)
+}
