d37821dc09beae9f89a0b6169692ee88f449e3a6
Pyro(JeongWan Gho) <rhwjddhks@gmail.com>
2025-11-11 00:18:20 +0900

    Revert "feat: createOrderCommand 에도 검증 로직 추가"
    This reverts commit b822a2388e8aaa1f04b6958073ccaeb3711f5f05.

--- apps/commerce-api/src/main/kotlin/com/loopers/domain/order/CreateOrderItemCommand.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/domain/order/CreateOrderItemCommand.kt
@@ -3,8 +3,4 @@ package com.loopers.domain.order
 data class CreateOrderItemCommand(
     val productId: Long,
     val quantity: Int,
-) {
-    init {
-        require(quantity > 0) { "주문 수량은 0보다 커야 합니다. 현재 값: $quantity" }
-    }
-}
+)
