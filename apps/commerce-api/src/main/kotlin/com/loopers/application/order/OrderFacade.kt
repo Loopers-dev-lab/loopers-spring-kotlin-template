a91d790b4fc7fdfb7ab473353c3c91fd17ed01e0
essaysir <sonjs7554@naver.com>
2025-12-05 00:50:55 +0900

    refactor: 주문 프로세스에 결제 연동
      - OrderFacade에서 결제 요청 및 결과 처리
      - 결제 실패 시 트랜잭션 롤백 처리
      - OrderCommand에 결제 정보 추가

--- apps/commerce-api/src/main/kotlin/com/loopers/application/order/OrderFacade.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/application/order/OrderFacade.kt
@@ -3,32 +3,51 @@ package com.loopers.application.order
 import com.loopers.domain.coupon.CouponService
 import com.loopers.domain.order.OrderModel
 import com.loopers.domain.order.OrderService
-import com.loopers.domain.point.PointService
+import com.loopers.domain.payment.PaymentService
+import com.loopers.domain.payment.dto.PaymentDto
 import com.loopers.domain.product.ProductService
+import com.loopers.support.error.CoreException
+import com.loopers.support.error.ErrorType
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
-import java.math.BigDecimal
 
 @Component
 class OrderFacade(
     private val orderService: OrderService,
-    private val pointService: PointService,
     private val productService: ProductService,
     private val couponService: CouponService,
+    private val paymentService: PaymentService,
 ) {
 
     @Transactional
-    fun order(userId: Long, couponId: Long?, command: OrderCommand): OrderModel {
+    fun order(userId: Long, command: OrderCommand): OrderModel {
+        // 1. 가격 계산
         val totalPrice = command.orderItems.sumOf { it.productPrice * it.quantity.toBigDecimal() }
 
-        val discountPrice = couponId?.let {
+        val discountPrice = command.couponId?.let {
             couponService.calculateDiscountPrice(it, userId, totalPrice)
-        } ?: BigDecimal.ZERO
+        } ?: totalPrice
 
-        pointService.pay(userId, totalPrice.minus(discountPrice))
-
+        // 2. 재고 차감
         productService.occupyStocks(command)
-        // 주문 생성
-        return orderService.order(userId, command)
+
+        // 3. 주문 생성 (PENDING)
+        val order = orderService.prepare(userId, command)
+
+        // 4. 결제 요청
+        when (
+            val result = paymentService.pay(
+                userId,
+                PaymentDto.Request.from(order.orderKey, command.cardType, command.cardNo, discountPrice),
+            )
+        ) {
+            is PaymentDto.Result.Success -> {
+                return orderService.requestPayment(order)
+            }
+
+            is PaymentDto.Result.Failed -> {
+                throw CoreException(ErrorType.INTERNAL_ERROR, result.errorMessage)
+            }
+        }
     }
 }
