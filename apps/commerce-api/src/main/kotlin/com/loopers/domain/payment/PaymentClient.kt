d4dabb9ff878dbfea99166b6099c9c0dfdc949dc
essaysir <sonjs7554@naver.com>
2025-12-07 11:09:17 +0900

    retry,circuit 누락 수정

--- apps/commerce-api/src/main/kotlin/com/loopers/domain/payment/PaymentClient.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/domain/payment/PaymentClient.kt
@@ -4,6 +4,8 @@ import com.loopers.domain.payment.dto.PaymentDto
 import com.loopers.interfaces.api.ApiResponse
 import com.loopers.support.error.CoreException
 import com.loopers.support.error.ErrorType
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
+import io.github.resilience4j.retry.annotation.Retry
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
@@ -14,8 +16,8 @@ class PaymentClient(private val paymentFeignClient: PaymentFeignClient) {
         private val logger = LoggerFactory.getLogger(PaymentClient::class.java)
     }
 
-    //    @Retry(name = "payment")
-//    @CircuitBreaker(name = "payment", fallbackMethod = "fallbackRequestPayment")
+    @Retry(name = "payment")
+    @CircuitBreaker(name = "payment", fallbackMethod = "fallbackRequestPayment")
     fun requestPayment(
         request: PaymentDto.Request,
     ): ApiResponse<PaymentDto.Response> {
@@ -30,6 +32,4 @@ class PaymentClient(private val paymentFeignClient: PaymentFeignClient) {
         logger.error("Payment circuit breaker activated for orderKey=${request.orderId}", throwable)
         throw CoreException(ErrorType.INTERNAL_ERROR, "결제 서비스 일시적 장애")
     }
-
-    fun request
 }
